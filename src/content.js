4595bdb762e9565741b953cb1661cd60a4c13bf4
IshantSRam <navrangidevi098@gmail.com>
2021-05-15 19:01:43 +0530

    Audio Toggle Added

--- src/content.js
+++ src/content.js
@@ -1,28 +1,76 @@
 // getting the aduio and video element in webpage
 const audioelm = document.getElementsByTagName("audio");
 const videoelm = document.getElementsByTagName("video");
-let volumelevel = 0;
+let volumelevel = 50;
 
-// every time reciving volumelevel
+// change volume Function
+const changeVolume = (volumelevel) => {
+  // itrating over the audioelm array
+  for (let audiodata of audioelm) {
+    // assining each element volume volumelevl
+    audiodata.volume = volumelevel;
+  }
+
+  // itrating over the videoelm array
+  for (let videodata of videoelm) {
+    // assining each element volume volumelevl
+    videodata.volume = volumelevel;
+  }
+
+  return;
+};
+
+// mute Volume Function
+const muteVolume = () => {
+  // itrating over the audioelm array
+  for (let audiodata of audioelm) {
+    // assining each element volume 0
+    audiodata.volume = 0;
+  }
+
+  // itrating over the videoelm array
+  for (let videodata of videoelm) {
+    // assining each element volume 0
+    videodata.volume = 0;
+  }
+
+  return;
+};
+
+const defaultState = () => {
+  // itrating over the audioelm array
+  for (let audiodata of audioelm) {
+    // assining each element volume 1
+    audiodata.volume = 1;
+  }
+
+  // itrating over the videoelm array
+  for (let videodata of videoelm) {
+    // assining each element volume 1
+    videodata.volume = 1;
+  }
+};
+
+// every time reciving message
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  // assining it to volume level var by dividing it by 100
+  // if message is true means audioState is on
+  if (request.message == true) {
+    // calling the defaultState Function
+    defaultState();
+
+    return;
+  }
+  // else if message is false means audioState is off
+  else if (request.message == false) {
+    // calling the muteVolume Function
+    muteVolume();
+
+    return;
+  }
+  // else assining it to volume level var by dividing it by 100
   volumelevel = request.message / 100;
+  // calling the chageVolume Function
+  changeVolume(volumelevel);
 
-  // if audio element length != 0
-  if (audioelm.length != 0) {
-    // itrating over the audioelm array
-    for (let audiodata of audioelm) {
-      // assining each element volume volumelevl
-      audiodata.volume = volumelevel;
-    }
-  }
-
-  // if video element length != 0
-  if (videoelm.length != 0) {
-    // itrating over the videoelm array
-    for (let videodata of videoelm) {
-      // assining each element volume volumelevl
-      videodata.volume = volumelevel;
-    }
-  }
+  return;
 });
--- src/script.js
+++ src/script.js
@@ -2,8 +2,20 @@
 const volume = document.getElementById("volume");
 let volumelevel = document.getElementById("volumelevel");
 
-// assining volumelevel HTML value volume.value
-volumelevel.innerHTML = volume.value;
+// getting Audio State tag
+const audioState = document.getElementById("audioState");
+let audioStateValue = document.getElementById("audioStateValue");
+
+// on audio On or Off state Input
+audioState.addEventListener("click", () => {
+  // assining audioStateValue innerHTML audioState.checked value
+  audioStateValue.innerHTML = audioState.checked ? "On" : "Off";
+  // sending the audioState to the content.js
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    var activeTab = tabs[0];
+    chrome.tabs.sendMessage(activeTab.id, { message: audioState.checked });
+  });
+});
 
 // on range input
 volume.oninput = function () {
@@ -13,6 +25,6 @@ volume.oninput = function () {
   // sending the volume.value to content.js
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     var activeTab = tabs[0];
-    chrome.tabs.sendMessage(activeTab.id, { message: volume.value });
+    chrome.tabs.sendMessage(activeTab.id, { message: parseInt(volume.value) });
   });
 };
